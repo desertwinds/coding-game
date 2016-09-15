1173f681c7e648af7091b668693866c6c57ed9e6
Javier-PC\Javier <javiantonini@gmail.com>
2016-09-15 15:32:03 +0100

    Downloaded new assets. Changed level prefabs. Started the door and switch tiles.

--- Assets/Scripts/LevelManagerScript.cs
+++ Assets/Scripts/LevelManagerScript.cs
@@ -6,17 +6,21 @@ public class LevelManagerScript : MonoBehaviour {
 
 	public int m_levels;
 	public GameObject m_levelButton;
-	public Transform scrollViewContent;
+	public RectTransform scrollViewContent;
 	public GameObject loadingImage;
+
+	private GridLayoutGroup m_grid;
 
 	void Awake () {
 		GameObject[] levels = Resources.LoadAll<GameObject> ("Levels");
 		m_levels = levels.Length;
+		m_grid = scrollViewContent.GetComponent<GridLayoutGroup> ();
 	}
 
 	void Start(){
 		GameObject button;
 		LevelButtonScript buttonScript;
+		float scrollContentHeight = scrollViewContent.sizeDelta.y;
 		for (int i = 1; i <= m_levels; i++) {
 			button = Instantiate (m_levelButton);
 			buttonScript = button.GetComponent<LevelButtonScript> ();
@@ -24,7 +28,6 @@ public class LevelManagerScript : MonoBehaviour {
 			buttonScript.text.text = "Level " + i;
 			buttonScript.loadingImage = loadingImage;
 			button.transform.SetParent (scrollViewContent);
-
 		}
 	}
 
